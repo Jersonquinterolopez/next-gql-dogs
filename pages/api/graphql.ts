490bdafab4467a036e02343eab22a03ca9e1e600
Jerson <solorsanolopez@gmail.com>
2022-04-05 14:13:32 -0700

    [Create] Dog objectType, resolver, schema

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,7 +1,28 @@
 import { ApolloServer } from "apollo-server-micro";
+import "reflect-metadata"
+import { buildSchema, Resolver, Query, Arg, ObjectType, Field, ID } from "type-graphql";
+
+@ObjectType()
+export class Dog {
+  @Field(type => ID)
+  name: string;
+}
+  
+@Resolver(Dog)
+export class DogResolver {
+    @Query(() => [Dog])
+    dogs() : Dog[] {
+        return [{name: "Rufus"}, {name: "Spot"}];
+    }
+}
+
+const schema = await buildSchema({
+    resolvers: [DogResolver],
+});
 
 // initialize apollo server
 const server = new ApolloServer({
+     schema
 });
 
 
